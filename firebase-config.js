2562fd8c090d9c1f7f7d21bd042e88f0d3be460e
officialorderaja <officialorderaja@gmail.com>
2025-06-04 17:27:21 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDp-srDmna3g34MXaR200ZsW7XOG7FwGrc",
+    authDomain: "memberorderaja.firebaseapp.com",
+    projectId: "memberorderaja",
+    storageBucket: "memberorderaja.firebasestorage.app",
+    messagingSenderId: "181500803408",
+    appId: "1:181500803408:web:ed07f33d6ae0293e615455",
+    measurementId: "G-KD7HMCGHGZ"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+window.firebaseApp = app;
+window.firebaseAuth = auth;
+window.firebaseDB = db;
