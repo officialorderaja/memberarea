544fc00c8d90e5b9c70376afe522170518c4583f
officialorderaja <officialorderaja@gmail.com>
2025-06-04 22:21:36 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
   const firebaseConfig = {
-  apiKey: "AIzaSyCTlWSmjHPpbRrsXurzorAXKfVEdi3GFVQ",
-  authDomain: "memberorderaja.firebaseapp.com",
-  projectId: "memberorderaja",
-  storageBucket: "memberorderaja.firebasestorage.app",
-  messagingSenderId: "181500803408",
-  appId: "1:181500803408:web:bcca622ac69800d3615455",
-  measurementId: "G-VJ7JXKMECQ"
+    apiKey: "AIzaSyCTlWSmjHPpbRrsXurzorAXKfVEdi3GFVQ",
+    authDomain: "memberorderaja.firebaseapp.com",
+    projectId: "memberorderaja",
+    storageBucket: "memberorderaja.firebasestorage.app",
+    messagingSenderId: "181500803408",
+    appId: "1:181500803408:web:bcca622ac69800d3615455",
+    measurementId: "G-VJ7JXKMECQ"
   };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-window.firebaseApp = app;
-window.firebaseAuth = auth;
-window.firebaseDB = db;
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  console.log("Firebase Auth initialized:", auth);
+</script>
